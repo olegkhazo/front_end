0becd8fe5282219658052541009effc166a7ffd7
Oleg Zhuravko <olegkhazo@gmail.com>
2021-10-26 18:38:33 +0300

    Задачки на соответствие числа с использованием && и ||

---
+++ lesson_12/lesson_12_2.js
@@ -0,0 +1,14 @@
+let num = 145;
+
+if (num >= 14 && num <= 90) {
+    console.log('It is okay');
+}
+
+
+if (num < 14 || num > 90) {
+    console.log('It is okay');
+}
+
+if (!(num >= 14 && num <= 90)) {
+    console.log('It is okay');
+}

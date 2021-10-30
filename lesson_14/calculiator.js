4633bb934a3407efcf9442d6282027b7dfb54839
Oleg Zhuravko <olegkhazo@gmail.com>
2021-10-30 20:50:55 +0300

    checkData function  edit

--- lesson_14/calculiator.js
+++ lesson_14/calculiator.js
@@ -1,33 +1,34 @@
-// Сбор данных
+// Забор данных
 function getData() { 
-    let first = +prompt("First Number", ""); 
-    let second = +prompt("Second Number", ""); 
-    let action = prompt("Action:\n One of this - sum, minus, multi, divide", "").toLocaleLowerCase(); 
- 
-    checkData(first, second, action); 
+  let first = +prompt("First Number", ""); 
+  let second = +prompt("Second Number", ""); 
+  let action = prompt("Action:\n One of this - sum, minus, multi, divide", "").toLocaleLowerCase(); 
+
+  checkData(first, second, action); 
 } 
- 
-// Проверка данных
+
+// Проверка
 function checkData(first, second, action) { 
-  if(!action || isNaN(first) || isNaN(second)) { 
-    return alert("Error"); 
-  } 
- 
-  if(!["minus", "sum", "multi", "divide"].includes(action)){ 
-    return alert("unknown operation"); 
-  } 
-   
-  computation(first, second, action);  
+if(!action || isNaN(first) || isNaN(second)) { 
+  return alert("You  must imput operands"); 
+} 
+
+if(!["minus", "sum", "multi", "divide"].includes(action)){ 
+  return alert("unknown operation"); 
 } 
  
+let res = computation(first, second, action); 
+alert(res) 
+} 
+
 // Вычисление
 function computation(first, second, action) { 
-  switch(action){ 
-    case "sum": return first + second; 
-    case "minus": return first - second; 
-    case "multi": return first * second; 
-    case "divide": return first / second; 
-  } 
+switch(action){ 
+  case "sum": return first + second; 
+  case "minus": return first - second; 
+  case "multi": return first * second; 
+  case "divide": return first / second; 
 } 
- 
+} 
+
 getData();

c2c3ff917bd2ea45bcafe66085c86185a8c8b7ba
Oleg Zhuravko <olegkhazo@gmail.com>
2021-11-09 14:46:39 +0200

    Vertical string

--- lesson_26.1/changeString.js
+++ lesson_26.1/changeString.js
@@ -1,7 +1,8 @@
 function showVerticalMessage(str) {
+    if ( !str ) return;
     str = str[0].toUpperCase() + str.slice(1, 10);
     for ( let i = 0; i < str.length; i++ ) {
         console.log(str[i]);
     }
 }
-showVerticalMessage('марафон');
+showVerticalMessage('');
